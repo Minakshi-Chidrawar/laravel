d4b11b6ae617b79d0702a52a2f02ff92beec9a54
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-03-25 20:09:34 +0000

    Added required attribute

--- websiteDatabase/resources/views/projects/create.blade.php
+++ websiteDatabase/resources/views/projects/create.blade.php
@@ -5,24 +5,34 @@
 
     <form method="POST" action="/projects">
         {{ csrf_field() }}
-        <div>
-            <label class="label" for="title">Title</label>
+        <div class="field">
+            <label class="label" for="title">Project Title</label>
 
             <div class="control">
-                <input type="text" class="input" name="title" placeholder="Project title">
+                <input type="text" class="input {{ $errors->has('title') ? 'is-danger' : ''}}" name="title" value="{{old('title')}}">
             </div>
         </div>   
 
-        <div>
-            <label class="label" for="description">Description</label>
+        <div class="field">
+            <label class="label" for="description">Project Description</label>
 
             <div class="control">
-                <textarea name="description" class="textarea" placeholder="Project description"></textarea>
+                <textarea name="description" class="textarea {{ $errors->has('description') ? 'is-danger' : ''}}" value="{{ old('description') }}"></textarea>
             </div>
         </div>   
 
-        <div>
+        <div class="field">
             <button type="submit">Create Project</button>
         </div>   
+
+        @if($errors->any())
+            <div class="notification is-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </form>
 @endsection
