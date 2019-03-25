fc907235cd650b3d298f4704f84175d63e7a9d87
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-03-25 20:10:26 +0000

    Tidy up

--- websiteDatabase/resources/views/projects/show.blade.php
+++ websiteDatabase/resources/views/projects/show.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     <h1 class="title">{{ $project->title }}</h1>
+
+    <div class="content">{{ $project->description }}</div>
+
+    <p>
+        <a href="/projects/{{ $project->id }}/edit">Edit</a>
+    </p>
 @endsection
