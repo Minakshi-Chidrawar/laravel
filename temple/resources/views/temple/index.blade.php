2e75ffc33ee84bc4847379a7c23061b7c71a7ec1
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-07 21:10:15 +0100

    Added stylesheet

--- temple/resources/views/temple/index.blade.php
+++ temple/resources/views/temple/index.blade.php
@@ -1,34 +1,38 @@
 @extends('master.layout')
 
 @section('content')
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+  </ul>
+  
+  <!-- The slideshow -->
   <div class="carousel-inner">
-    <div class="carousel-item active">
-        <img class="d-block w-100" src="{{ asset('images/Ganesha.png') }}" alt="First slide">
-        <div class="carousel-caption d-none d-md-block">
-            <h5>Ganesha</h5>
-            <p>Jay Ho</p>
-        </div>
+    <div class="carousel-item carousel-img active">
+      <img src="{{ asset('images/Mataji.png') }}" alt="Los Angeles">
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="{{ asset('images/Mataji.png') }}" alt="Second slide">
+    <div class="carousel-item carousel-img">
+      <img src="{{ asset('images/Mataji1.png') }}" alt="Chicago">
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="{{ asset('images/Hanuman.png') }}" alt="Third slide">
+    <div class="carousel-item carousel-img">
+      <img src="{{ asset('images/Mataji2.png') }}" alt="New York">
+    </div>
+    <div class="carousel-item carousel-img">
+      <img src="{{ asset('images/Mataji3.png') }}" alt="New York">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
   </a>
 </div>
 @endsection
