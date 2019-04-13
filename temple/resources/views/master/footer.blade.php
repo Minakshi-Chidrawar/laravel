21bc9283ae9f9d6ff6a5c5628930af194d637315
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-13 22:21:56 +0100

    Updated for icons

--- temple/resources/views/master/footer.blade.php
+++ temple/resources/views/master/footer.blade.php
@@ -9,16 +9,16 @@
 
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-0">
-
           <!-- Content -->
           <h5 class="font-weight-bold text-uppercase mb-8">Timings: </h5>
-          <p>Open daily 08:00-18:30 PM<br>Aarti daily at 12:00 noon and 18:00</p>
-
+            <ul class="list-unstyled">
+              <li>Open daily 08:00-18:30 PM</li>
+              <li>Aarti daily at 12:00 noon and 18:00</li>
+            </ul>
         </div>
         <!-- Grid column -->
 
         <hr class="clearfix w-100 d-md-none">
-
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
 
@@ -53,16 +53,16 @@
 
           <ul class="list-unstyled">
             <li>
-                <i class="fas fa-home mr-3"></i> Adhya Shakti Mataji Temple
+                Adhya Shakti Mataji Temple
             </li>
             <li>
-                <i class="fas fa-envelope mr-3"></i> 55, High Street, Cowley, Uxbridge
+                55, High Street, Cowley, Uxbridge
             </li>
             <li>
-                <i class="fas fa-envelope mr-3"></i> Middlesex UB8 2DZ
+                Middlesex UB8 2DZ
             </li>
             <li>
-                <i class="fas fa-print mr-3"></i> 078 8225 3540
+                <i class="fa fa-phone"></i> 078 8225 3540
             </li>
           </ul>
 
@@ -78,22 +78,10 @@
           <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
 
           <!-- Facebook -->
-          <a type="button" class="btn-floating btn-fb">
-            <i class="fab fa-facebook-f"></i>
-          </a>
+            <a href="https://www.facebook.com/adhyashakti.mataji" class="fa fa-facebook"></a>
           <!-- Twitter -->
-          <a type="button" class="btn-floating btn-tw">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <!-- Google +-->
-          <a type="button" class="btn-floating btn-gplus">
-            <i class="fab fa-google-plus-g"></i>
-          </a>
-          <!-- Dribbble -->
-          <a type="button" class="btn-floating btn-dribbble">
-            <i class="fab fa-dribbble"></i>
-          </a>
-
+            <a href="https://twitter.com/Matajitempleuk" class="fa fa-twitter"></a>
+            <a href="https://adhyashaktimatajitemple.wordpress.com" class="fa fa-wordpress"></a>
         </div>
         <!-- Grid column -->
 
