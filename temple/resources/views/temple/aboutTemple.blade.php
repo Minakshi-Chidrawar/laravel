512751115511e077bcf9eb78b90677fede122e8c
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-14 23:20:03 +0100

    Added About temple view

---
+++ temple/resources/views/temple/aboutTemple.blade.php
@@ -0,0 +1,37 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>About Temple</h1>
+    <div class="bs-callout">
+        <p>
+            Inspired by our Mother, Gulabben Gangji Maicha, in 1970 celebrating, Navratri with Garba and Dandiya Raas at her residence in Southall. The event was so popular that within a couple of days the back garden was too small.
+        </p>
+        <p>
+            A suitable hall, The New Hall, located on the High Street, Southall was found and thus the beginning of “Navratri Garba”. Everyone, irrelevant of Cast, Creed or Background was welcome. The Navratri celebrations took place in various locations including Southall, Hayes and Perivale until finally moving to the Arya Samaj, Argyle Road in 1993. The Arya Samaj became a place for Navratri Celebrations for years.
+        </p>
+        <p>
+            The funds and donations collected were distributed to other Charities in the aid of free food distribution, BBC children in need, Blue Peter water appeal, Cry with Children, British Heart Foundation, Animal sanctuaries, free medicines to the poor, National Asthma Campaign, Mental Health Foundation, Cancer Research UK, Tuberous Sclerosis Asso. British Red Cross, British Diabetics Asso. Etc. Donations were made in UK and overseas where help was required.
+        </p>
+        <p>
+            After the earthquake in Kutch, where our services and donations helped several hundred, we became a registered charity. This was the starting point and we have not looked back ever since. We committed ourselves to helping out wherever we could and raised funds to various disasters. This was the time when we decided to open a Mataji temple so that people from all walks of life could benefit from it, meet and make friends, take part in festival celebrations, participate in prayers and Havans and enjoy free activities and food from time to time.
+        </p>
+        <p>
+            Whilst our charity work continues, we are looking to extend the temple. The temple alone will be a sanctuary where people will achieve piece tranquillity and happiness. There will be an opportunity for people to learn, meet other people and socialise.
+        </p>
+        <p>
+            The temple will also contain a library with books of Vedas, Purans, Upanishads, Bhajans and a vast range of other interesting books for the benefit of visitors and have a reading room.
+        </p>
+        <p>
+            It is also hoped that there will be language classes in Gujarati, Hindi and Sanskrit for children as well as adults and English for adults and children who wish to participate.
+        </p>
+        <p>
+            These social gatherings are very important not only for reasons mentioned above but for the up keep of our old values and traditions for the future generations in learning from the various cultural activities and programmes. We will organize talks on subjects such high blood pressure, diabetics, first aid etc and also hold bhajans, show films, entertain and serve meals on a regular basis.
+        </p>
+        <p>
+            The mandir will be a sanctuary where people can come to try to solve their problems and/or share their views and have pleasant, stimulating and friendly debates/discussions. Lonely elderly people can come for their meals as an alternative to having meals on wheels.
+        </p>
+        <p>
+            The temple will also have a temporary guest room for people from overseas to stay if they have nowhere else to stay. The temple will also be publishing a quarterly newspaper, for the benefit of members both in UK and overseas.
+        </p>
+    </div>
+@endsection    
