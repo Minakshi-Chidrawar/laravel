3bbcdaee80987bb29bc186ea471cfab2fd5067b5
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-15 21:08:34 +0100

    Added new page

---
+++ temple/resources/views/temple/mission.blade.php
@@ -0,0 +1,43 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>Our Vision & mission</h1>
+    <div class="bs-callout">
+        <p>
+            The Vision of Adhya Shakti Mataji Temple is to enhance the religious experience for all in our diverse congregational community. The temple will strive to conduct religious, spiritual, educational, social and cultural activities for the benefit of the Hindu community and the wider interested audience.
+        </p>
+        <p>
+            Temple will seek constantly ways and means to preserve and embellish Hindu traditions & cultural heritage, promote the values of peace, harmony, human values of truth, tolerance, non violence, forgiveness, respect for one and another and sharing through the means of prayer, Bhajans and community spirit.
+        </p>
+        <p>
+            Adhya Shakti Mataji Temple and with Mataji’s blessings to realise our dream of a beautiful Shrine to house the deities in front of the current Hindu Temple in Cowley, Uxbridge.
+        </p>
+        <h4>The purpose of the Adhya Shakti Mataji Temple would be:</h4>
+        <ul class="font-color">
+            <li>
+                To be a House of Worship Goddess Durga - Amba Maa, to promote and practice ideals of Hinduism and Hindu religion through Worship, education and teachings.
+            </li>
+            <li>
+                To constantly strive for spiritual richness and human excellence through assimilation of values in Hindu scriptures into daily lives.
+            </li>
+            <li>
+                To develop activities to encourage and foster interfaith understanding and cooperation.
+            </li>
+            <li>
+                To recognize and respect other religions and belief systems in their proper context.
+            </li>
+        </ul>
+        <p>
+            Through this focus, it is our mission to be mindful of our Hindu heritage, develop a congregational sense of responsibility to the extended community we serve and create an atmosphere of extended family for all our members and by serving Maha Prasad. And for these purposes construct expand and operate a Hindu Temple in Cowley, Uxbridge.
+        </p>
+        <h4>Charitable Objects:</h4>
+        <ul class="font-color">
+            <li>
+                The advancement of Hindu Religion, particularly by the performance of Navratri Garba religion celebrations (Including Havan of Godess Durga and Amba Maa), indegenous to Gujaratis.        
+            </li>
+            <li>
+                The relief of poverty, sickness and distress amongst those who may be in need of help in the United Kingdom and overseas.
+            </li>
+        </ul>
+    </div>
+@endsection
