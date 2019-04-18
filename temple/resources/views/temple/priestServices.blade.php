92e0918161467600686864b8fbd3ec71aa78c09f
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-18 23:39:42 +0100

    Added priestService page

---
+++ temple/resources/views/temple/priestServices.blade.php
@@ -0,0 +1,22 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>Priest Services</h1>
+    <div class="bs-callout">
+        <p style="text-align:center; margin-left:15%; margin-right:15%;"><b>General Register Office has certfied Adhya Shakti Mataji Temple as a "Place for Religious Worship" and authorised for solemnizing Marriages therein.</b></p>
+        <table class="div-table4">                            
+            <tr>
+                <td class="btn-service">a. <a href="InTempleAct.html">
+                    In-Temple Religious Activities</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="btn-service">b. <a href="OutTempleAct.html">
+                    Out of Temple Activities - At your home or at requested place</a>
+                </td>
+            </tr>
+
+        </table>
+    </div>
+@endsection
