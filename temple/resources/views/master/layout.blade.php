7218050b6f3ca71c7e94007f96cde16780e10e97
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-19 23:12:54 +0100

    Added ref for Aarati page

--- temple/resources/views/master/layout.blade.php
+++ temple/resources/views/master/layout.blade.php
@@ -12,37 +12,7 @@
 
 <body>
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{route('home')}}">Shree Adhya Shakti Mataji</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="aarti.html">Aartis</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('priestServices')}}">Priest Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="events.html">News & Events</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="gallery.html">Gallery</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('mission')}}">Mission</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('donation')}}">Donations</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    @include('master.nav')    
 
     <div class="container">
         @yield('content')
