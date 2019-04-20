7f02cf923d0e6d39003c55ca188ba5b828dd26f5
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-20 23:20:25 +0100

    Amended for Audio source

--- temple/resources/views/temple/aarati.blade.php
+++ temple/resources/views/temple/aarati.blade.php
@@ -5,12 +5,7 @@
 <div class="bs-callout">
     <div class="row">
         <div class="col-md-3">
-            <div class="nav flex-column" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">|| Ganesha Shlok ||</a>
-                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">|| Gayatri Mantra ||</a>
-                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">|| Mataji aarati ||</a>
-                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">|| Hanuman Chalisa ||</a>
-            </div>
+            @include('partials.verticalNav')
         </div>
         <div class="col-md-8">
             <div class="center-heading text-center wow animated fadeInUp" data-wow-duration="700ms" data-wow-delay="300ms">
@@ -31,11 +26,5 @@
             </div>
         </div>
     </div>
-    <!-- <div class="tab-content" id="v-pills-tabContent">
-        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-    </div> -->
 </div>
 @endsection
