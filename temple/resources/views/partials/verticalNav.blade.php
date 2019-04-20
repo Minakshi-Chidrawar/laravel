ba52df53113f777a0febba8c4a3619cea47d6731
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-20 23:14:25 +0100

    Added vertical navigation

---
+++ temple/resources/views/partials/verticalNav.blade.php
@@ -0,0 +1,6 @@
+<nav class="nav flex-column">
+  <a class="nav-link active" href="{{route('aarati')}}">|| Ganesha Shlok ||</a>
+  <a class="nav-link" href="{{route('gayatriMantra')}}">|| Gayatri Mantra ||</a>
+  <a class="nav-link" href="{{route('mataji')}}">|| Mataji aarati ||</a>
+  <a class="nav-link" href="{{route('hanumanChalisa')}}">|| Hanuman Chalisa ||</a>
+</nav>
