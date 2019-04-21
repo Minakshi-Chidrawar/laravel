1b86748ff916cb81646095e20d61c872722abbce
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-21 23:22:52 +0100

    Added view for out temple activities

---
+++ temple/resources/views/temple/outTempleActivities.blade.php
@@ -0,0 +1,49 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>Priest Services</h1>
+    <div class="bs-callout">
+        <p style="text-align:center;"><b>General Register Office has certfied Adhya Shakti Mataji Temple as a "Place for Religious Worship" and authorised for solemnizing Marriages therein.</b></p>
+        <div class="row">
+            <div class="col-md-3">
+                @include('partials.verticalPriestServiceNav')
+            </div>
+            <div class="col-md-8">
+                <table class="templeActivities">
+                    <tr>
+                        <td class="col1">Funeral Ceremony</td>                                    
+                        <td>A funeral is a ceremony marking a person's death. Funerary customs comprise the complex of beliefs and practices used by a culture to remember the dead, from the funeral itself, to various monuments, prayers, and rituals undertaken in their honor. These customs vary widely between cultures, and between religious affiliations within cultures. In some cultures the dead are venerated; this is commonly called ancestor worship. The word funeral comes from the Latin funus, which had a variety of meanings, including the corpse and the funerary rites themselves.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Wedding Ceremony</td>                                    
+                        <td>A wedding is a ceremony that celebrates the beginning of a marriage or civil union. Wedding traditions and customs vary greatly between cultures, ethnic groups, religions, countries, and social classes. In some countries, cultures and religions, the actual act of marriage begins during the wedding ceremony.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Griha Shanti</td>                                    
+                        <td>Griha Shanti Puja After having matched the horoscopes of the prospective bride and groom, a puja or prayer is held. It is called Griha Shanti puja, which is done by the priest. A Mahurat or auspicious time is chosen for the conduct of the prayer. All the family members and relatives participate in this auspicious ritual. It is an important puja, which is performed at the bride as well as groom's house.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Gruha Pravesh</td>                                    
+                        <td>Kalash Poojan is to be performed just as soon as you take possession of your house before moving in your household items. It is also recommended for rental house. It is performed as a nice and auspicious beginning of your tenure in that property.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Vastu Puja</td>                              
+                        <td>Vastu puja is considered to be very important, if you are exercising vastu at your place. When you buy a new house or shop then this puja is compulsory as this eliminates all negative energies. This puja once done helps you to do well in business and promotes peace in the house. This vidhi done by Tantrik procedure is very effective although very simple. There is a special puja to be done which can be done at our place or you can get this pooja done at your tantrik's place.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Nam Sanskar</td>                                    
+                        <td>In all cultures around the world just after a child is born there's an official naming ceremony that is to be performed (Baptism, Christening, etc.); Nama karana, or name giving, in which the father officially and publically declares the name of the child for all to know and use.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Satya Narayan Puja<br> (Katha )</td>                                    
+                        <td>The Satyanarayan Puja is performed in reverence to the Narayan form of Lord Vishnu. The Lord in this form is considered an embodiment of truth. This puja is conducted to ensure abundance in ones life. Many people carry out this puja immediately after or along with an auspicious occasion like a marriage or moving into a new house or any other success in life. It is believed the ceremony originated in Bengal as Satya Pir and was later adapted into Satyanarayan puja.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Durga Puja</td>                                    
+                        <td>Durga Puja is celebrated with joy all over India, especially West Bengal in worship of Goddess Durga. Singing, dancing, sweets & gaiety are an integral part of the Durga Pooja Festival. People send Durga Puja Gifts to dear ones and express good wishes.</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
