ad761cc278981a157cf1840f78fd7221d13365d1
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-21 23:21:06 +0100

    Added navigation for Priest services

---
+++ temple/resources/views/partials/verticalPriestServiceNav.blade.php
@@ -0,0 +1,4 @@
+<nav class="nav flex-column">
+  <a class="nav-link active" href="{{route('inTempleActivities')}}">In Temple Religious Activities</a>
+  <a class="nav-link" href="{{route('outTempleActivities')}}">Out of Temple Activities</a>
+</nav>
