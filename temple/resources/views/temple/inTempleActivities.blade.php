e9c25fbe1d890294ed60c9901073c95ade197f11
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-21 23:22:05 +0100

    Added view for in temple activities

---
+++ temple/resources/views/temple/inTempleActivities.blade.php
@@ -0,0 +1,49 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>Priest Services</h1>
+    <div class="bs-callout">
+        <p style="text-align:center;"><b>General Register Office has certfied Adhya Shakti Mataji Temple as a "Place for Religious Worship" and authorised for solemnizing Marriages therein.</b></p>
+        <div class="row">
+            <div class="col-md-3">
+                @include('partials.verticalPriestServiceNav')
+            </div>
+            <div class="col-md-8">
+                <table class="templeActivities">
+                    <tr>
+                        <td class="col1">All Kind of Havans</td>                                    
+                        <td>The ritual consists of prayers, chanting, offerings, etc. These are done with a team of dasas. A Havan is a sacred purifying ritual (yajna) in Hinduism that involves a fire ceremony. It is a ritual of sacrifice made to the fire god Agni. After lighting a Havan Kund (sacrificial fire), objects such as fruits, honey, or wooden goods are put into the sacred fire.If there are any spirits that are evil around you or even inside you they get burned off in the sacred fire.It is believed that this sacrifice will bring health, happiness, luck and prosperity.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Griha Shanti</td>                                    
+                        <td>Griha Shanti Puja After having matched the horoscopes of the prospective bride and groom, a puja or prayer is held. It is called Griha Shanti puja, which is done by the priest. A Mahurat or auspicious time is chosen for the conduct of the prayer. All the family members and relatives participate in this auspicious ritual. It is an important puja, which is performed at the bride as well as groom's house.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Maha Laxmi Puja</td>                                    
+                        <td>Lakshmi, also called Laxmi, is the goddess of wealth,fortune, power, luxury, beauty, fertility, and auspiciousness. She holds the promise of material fulfilment and contentment. She is described as restless, whimsical yet maternal, with her arms raised to bless and to grant her blessings.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Durga Puja</td>                                    
+                        <td>Durga Puja is celebrated with joy all over India, especially West Bengal in worship of Goddess Durga. Singing, dancing, sweets & gaiety are an integral part of the Durga Pooja Festival. People send Durga Puja Gifts to dear ones and express good wishes.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Masik - Shivratri<br>(Shiv Abhishek) Puja</td>                              
+                        <td>Shivaratri Pooja has been given tremendous significance in Hindu mythology. It is said that ritual worship of Lord Shiva on a Shivaratri day pleases Lord Shiva the most. Devotees further believe that by pleasing Lord Shankara on the auspicious Shivaratri day, a person is absolved of past sins and is blessed with Moksha or salvation.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Hanuman Chalisa</td>                                    
+                        <td>The Hanuman Chalisa is a very powerful mantra and many a sage have contemplated on the same to provide their own dimensions and meaning to the Chalisa. The Chalisa was actually written by Sant Tulsidas and is in the old Hindi style. The Hanuman Chalisa has always been revered as the most sacred and powerful mantra and has certainly helped me in gaining my stature during times of weakness.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Bhajans</td>                                    
+                        <td>Songs of devotion sung in praise of God, usually in groups. A Bhajan or kirtan is a Hindu devotional song, often of ancient origin. Great importance is attributed to the singing of bhajans with Bhakti, i.e. loving devotion. "Rasanam Lakshanam Bhajanam" means the act by which we feel closer to either our true self (see jiva, atman), or to God (see Brahman& Ishvara). Acts which are performed for the purpose of pleasing God are also sometimes called bhajan.</td>
+                    </tr>
+                    <tr>
+                        <td class="col1">Devi Bhagwat and <br>Bhagwat Katha</td>                                    
+                        <td>Shrimad Bhagwat liberates us from fear, problems, and ignorance. In essence, this is the crux of Shrimad Bhagwat. It is a reminder of the very principles that guide & regulate our lives. He explained those principles in a very interesting manner picking up stories from the lives of people in the history as well as contemporary times. He gave numerous examples how these principles were upheld by great saints & souls for thousands of years. He cautioned the audience that these stories & incidents are only the conveying tools for the real message.</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
