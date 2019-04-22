1add11955c0f0489f411dda29203b53ac2133b63
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-22 20:17:40 +0100

    Minor changes

--- temple/resources/views/temple/donation.blade.php
+++ temple/resources/views/temple/donation.blade.php
@@ -7,7 +7,7 @@
             There is a saying - <b>There is no better donation than feeding a soul.</b> You can help us by donating money and/or your volunteering service in temple, in office administration, collection of charitable donations and spreading the word about our mission.
         </p>
         <p>
-            As the <a href="images/Proposed_Temple_Plan.jpg"  target="_blank">extension of the temple</a> is being finalised we are looking for funding and sponsorships. Charity begins at home but should not stop there. An extension means more space opening doors to more activities for the elderly and children. Our music classes, language classes, singing classes and yoga sessions will be more comfortable. We will be able to accommodate more people during the festive season, thus moving forward. We are actively raising funds to ensure we can do all we can for the less fortunate and the elderly.
+            As the <a href="images/Proposed_Temple_Plan.jpg" target="_blank">extension of the temple</a> is being finalised we are looking for funding and sponsorships. Charity begins at home but should not stop there. An extension means more space opening doors to more activities for the elderly and children. Our music classes, language classes, singing classes and yoga sessions will be more comfortable. We will be able to accommodate more people during the festive season, thus moving forward. We are actively raising funds to ensure we can do all we can for the less fortunate and the elderly.
         </p>
         <p>
             The day after Navratri, that is the 10th day after Ashwina, is Dussera when we celebrate the victory of Lord Rama over Ravana. Ravana is burnt in his effigy; often, giant dummies of Ravana stuffed with fireworks are shot with arrows until they blow up before a large, applauding audience.
@@ -37,11 +37,11 @@
                         <img alt="" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
                     </form>
                 </li>
-                <li type="a">easyfundraising: <a href="easyfundraising.html" title="easyfundraising"><b><u>Procedure</u></b></a></li>
+                <li type="a">easyfundraising: <a href="{{route('easyFundRaising')}}" title="easyfundraising"><b><u>Procedure</u></b></a></li>
             </ul>
         </p>
         <p>
-            <img src="images/giftaid.png" width="122" height="48" onload="OnLoadPngFix()" title="" alt="" style="float:left; margin:6px 6px 6px 3px">Gift Aid it and make your donation worth more to Mataji&#39;s Temple at no extra cost to yourself! Download <a href="wpdocs/Gift_Aid.pdf"  target="_blank">Gift Aid Declaration Form</a> and send it to us along with your donation.
+            <img src="images/giftaid.png" width="112" height="48" alt="Gift Aid" class="giftaid">Gift Aid it and make your donation worth more to Mataji&#39;s Temple at no extra cost to yourself! Download <a href="wpdocs/Gift_Aid.pdf"  target="_blank">Gift Aid Declaration Form</a> and send it to us along with your donation.
         </p>
         <p>
             Our Registered Charity can claim back the tax paid on donations from the HMRC. The money that you donate to the charity is money that you have already paid tax on, so by Gift- Aiding it, we can reclaim that tax. Gift Aid is calculated at the basic- rate taxpayer’s pay, which is currently 20%.
@@ -57,8 +57,8 @@
         </p>
         <p>
             <ul type="disc">
-                <li>Items required for temple: <b><u><a href="wpdocs/Mandir Requirements.pdf"  target="_blank">List</a></u></b></li>
-                <li>Temple's extension - <a  class="btn-service" href="templextension.html" style="color:#ffffff;">Details of extension</a></li>
+                <li>Items required for temple: <a href="wpdocs/Mandir Requirements.pdf"  target="_blank">List</a></li>
+                <li>Temple's extension - <a  class="btn-service" href="{{route('templeExtension')}}">Details of extension</a></li>
             </ul>
         </p>
         <p class="thank-you">
