343600e1ad31aeb1658612ba5c896bb84bfb0377
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-22 20:17:53 +0100

    Minor changes

--- temple/resources/views/temple/horoscope.blade.php
+++ temple/resources/views/temple/horoscope.blade.php
@@ -5,23 +5,28 @@
 
     <div class="bs-callout">
         <p>
-            We are pleased to announce that Shri Adhya Shakti Mataji Temple’s deeply experienced and intuitive Vedic Astrologers can now provide you accurate Vedic Horoscope.
+            We are pleased to announce that <strong>Shri Adhya Shakti Mataji</strong> Temple’s deeply experienced and intuitive Vedic Astrologers can now provide you accurate Vedic Horoscope.
         </p>
-        <p class="hidden-xs">                            
-            <b>Vedic astrology</b> is about 7000 years old and the oldest branch of astrology known. This science has been mentioned in the Vedas and carried down through generations. Vedic Astrology uses a unique system of dasas and sub divisional charts that are different from Western Astrology. It can give astonishing results when an expert Vedic scholar reads your chart based on your correct date, place and time of birth.
-        <p class="hidden-xs">
-            <img src="images/horoscope.png">
-            A birth chart (also known as Janma Kundali and Vedic horoscope) captures the precise astronomical positions of stars and planets at the individual&#39;s birth moment. Using the vast amounts of information provided by your birth chart, Vedic Astrology could predict every important event and possible opportunity that can arise in your life. Horoscope is the construction of your basic placement of planets at time of birth in a pre-<wbr>decided format called the birth chart . There are basic differences in techniques in casting birth charts by Indian Vedic, Western, Greek, Chinese System etc. It is this differences which makes the interpretations &amp; predictions different and superior to other systems.
+        <p>                            
+            <strong>Vedic astrology</strong> is about 7000 years old and the oldest branch of astrology known. This science has been mentioned in the Vedas and carried down through generations. Vedic Astrology uses a unique system of dasas and sub divisional charts that are different from Western Astrology. It can give astonishing results when an expert Vedic scholar reads your chart based on your correct date, place and time of birth.
         </p>
-        <p class="hidden-xs">
-            <b>How the Indian Vedic Horoscope is superior to other system: </b>The birth chart cast on the Indian System is truly unique since the Indian system makes adjustments for the fact that our Zodiac / Universe is moving and not fixed ( by the Big Bang theory of creation of Universe - this system is following for more than 7000 years, proving ancient Indians had a great grasp of astronomy before any other civilization).
+        <div class="row">
+            <div class="col-md-3">
+                <img src="images/horoscope.png" width="250px;" height="200px;">
+            </div>
+            <div class="col-md-8">
+                A birth chart (also known as Janma Kundali and Vedic horoscope) captures the precise astronomical positions of stars and planets at the individual&#39;s birth moment. Using the vast amounts of information provided by your birth chart, Vedic Astrology could predict every important event and possible opportunity that can arise in your life. Horoscope is the construction of your basic placement of planets at time of birth in a pre-<wbr>decided format called the birth chart . There are basic differences in techniques in casting birth charts by Indian Vedic, Western, Greek, Chinese System etc. It is this differences which makes the interpretations &amp; predictions different and superior to other systems.
+            </div>
+        </div>
+        <p class="sign-off">
+            <strong>How the Indian Vedic Horoscope is superior to other system: </strong>The birth chart cast on the Indian System is truly unique since the Indian system makes adjustments for the fact that our Zodiac / Universe is moving and not fixed (by the Big Bang theory of creation of Universe - this system is following for more than 7000 years, proving ancient Indians had a great grasp of astronomy before any other civilization).
         </p>
         <p>
             Also the Indian system takes into account that the zodiac is epileptic an not circular due to which the twelve houses in a birth chart are not 30 degree each but varying according to the time of birth. Due to these differences the birth chart cast on the Indian system would come out different from that done on any other system AND much more accurate.
         </p>
-        <p>
-            <b style="margin-left: 10%;">Get your Vedic Horoscope now from our expert and experienced astrologer from Shri Adhya Shakti Mataji Temple</b><br>
-            <b style="margin-left: 45%;"><a href="vedichoro.html">Click here</a></b>
+        <p style="text-align: center;">
+            <strong>Get your Vedic Horoscope now from our expert and experienced astrologer from Shri Adhya Shakti Mataji Temple</strong><br>
+            <strong><a href="vedichoro.html">Click here</a></strong>
         </p>
     </div>
 
