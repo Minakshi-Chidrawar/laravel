7696cf3a470f7da0759fb5185e36dea20db3c682
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-22 20:11:12 +0100

    Added page for the description of procedure for easyfundraising

---
+++ temple/resources/views/temple/easyFundRaising.blade.php
@@ -0,0 +1,12 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>Procedure for easyfundraising</h1>
+
+    <div>
+        <p style="margin-left: 23%;">Support raising more donation for Mataji’s Temple through easyfundraising.</p>
+            <iframe src="http://wfyw.easyfundraising.org.uk/?id=127729" width="100%" height="2000" frameborder="0" scrolling="no">
+            </iframe>
+        </p>
+    </div>
+@endsection
