a66afb7d2eed8a2a0166f5d90276c8159493900f
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-22 20:14:07 +0100

    Added page for the details about temple extension

---
+++ temple/resources/views/temple/templeExtension.blade.php
@@ -0,0 +1,43 @@
+@extends('master.layout')
+
+@section('content')
+    <h1>Temple Extension</h1>
+
+    <div class="bs-callout">
+        <p>
+            We are delighted to let you know that the Hillingdon Council has now approved<a href="docs/Planning Granted May 2014.pdf"  target="_blank"> our plans</a> for a substantial extension at the rear and side of the temple to build a new kitchen, toilets, store room will figure in the first phase of development. The second stage will involve construction of the Shrine in front of the temple and it will be on the lines of the image presented.    
+        </p>
+        <p>                            
+            This development will give storage facility, prepare Maha Prasad for Mataji, for priests and devotees. It goes without saying that future generations stand to benefit from the presence of an improvised sacred building. Of course, good work of this magnitude can be done only with substantial investment.
+        <p>
+            Background and Current Position:  Adhya Shakti Mataji Temple with its magnificent Goddess Amba seated majestically inside, was inaugurated on 15th November 2007. First Phase total cost including materials is approximately £260,000 and we are short of £160,000. It is in this connection that I beseech your support to realise our dream for our beloved Goddess Amba Ma or Mataji. The starting date for the first phase is 4th Sept, 2014. The second phase of the shrine will cost around £250,000.
+        </p>
+        <p>
+            We humbly request your support in the form of one-off donations or regular monthly contributions to realise our long-cherished goal. Over and above this, if you are able to provide any form of support to our project, you are more than welcome to do so.
+        </p>
+        <p>
+            Financial contribution can be made directly to temple’s account whose detail is as follows: Adhya Shakti Mataji Temple, Bank: HSBC Bank Plc, Sort Code: 40-03-09, Account Number: 61373544. Alternatively, you can pay online via PayPal or send a cheque in the name of Adhya Shakti Mataji Temple and post it to the 55, High Street, Cowley, Uxbridge, Middlesex UB8 2DZ.
+        </p>
+        <p> 
+            Please click below links for <strong>proposed plan</strong> for temple's extension:<br>
+            <a href="docs/side_extension1.pdf" target="_blank">Side Extension Drawing 1</a><br>
+            <a href="docs/side_extension2.pdf" target="_blank">Side Extension Drawing 2</a><br>
+            <a href="docs/side_extension3.pdf" target="_blank">Side Extension Drawing 3</a><br>
+            <a href="docs/Visual of Mataji Temple.pdf" target="_blank">Visual of Mataji Temple</a><br>
+        </p>
+        <p> 
+            Please click for <a href="docs/Pictures of Building Works Phase 2.pdf" target="_blank">
+            <strong>current phase</strong></a> of temple's extension:<br>
+        </p>                            
+
+        <p>
+            <strong>How can you help us?</strong><br>
+            You can help us by donating money, by becoming a volunteer, both for office administration, collection of charitable donations and spreading the word about our mission. A lot of help is needed, especially at this stage so please come forward and give generously.</p>
+        <p>
+            Please use attached <a href="docs/Fundraising Form Fighting Fund.pdf"  target="_blank">Fundraising Form </a>to donate. You can also make online donation on our website. Please also submit your gift aid form, if applicable. For more detail click on <a href="{{route('donation')}}">Donation</a>.
+        </p>
+
+        <p class="sign-off">Your kindness is highly appreciated.<br>Thank you very much.</p>
+        <p><strong>Jai Mataji.</strong></p>
+    </div>
+@endsection
