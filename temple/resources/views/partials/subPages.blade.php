6f011aead4e2765b70480662c3691161a8782f2b
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-28 21:06:59 +0100

    Added sub columns for Index page

---
+++ temple/resources/views/partials/subPages.blade.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-4">
+      <div class="subPages">
+        <h5 class="font-weight-bold mb-8">Timings</h5>
+        <ul class="list-unstyled">
+          <li>Open daily 08:00-18:30 PM</li>
+          <li>Aarti daily at 12:00 noon and 18:00</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col-4">
+      <div class="subPages">
+        <h5 class="font-weight-bold mb-8">Gallery</h5>
+      </div>
+    </div>
+
+    <div class="col-4">
+      <div class="subPages">
+        <h5 class="font-weight-bold mb-8">News & Events</h5>
+      </div>
+    </div>
+  </div>
