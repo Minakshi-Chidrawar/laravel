71f93ea6b768d2e5257fe4140bebbc470e3cb1b4
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-04-29 20:39:06 +0100

    Added Event

--- temple/resources/views/partials/subPages.blade.php
+++ temple/resources/views/partials/subPages.blade.php
@@ -12,12 +12,20 @@
     <div class="col-4">
       <div class="subPages">
         <h5 class="font-weight-bold mb-8">Gallery</h5>
+        <ul class="list-unstyled">
+          <li>Coming Soon...</li>
+          <li>&nbsp;</li>
+        </ul>
       </div>
     </div>
 
     <div class="col-4">
       <div class="subPages">
         <h5 class="font-weight-bold mb-8">News & Events</h5>
+        <ul class="list-unstyled">
+          <li>You can also find events for {{ date('Y') }} at temple: <a href="docs/Calendar 2017.pdf">Click</a></li>
+          <li>Coming Event: </li>
+        </ul>
       </div>
     </div>
   </div>
