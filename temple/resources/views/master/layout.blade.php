b4537f7b812b70d83104854a1af52496bfd00a8f
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-14 21:03:45 +0100

    Moved script tag at the bottom

--- temple/resources/views/master/layout.blade.php
+++ temple/resources/views/master/layout.blade.php
@@ -4,7 +4,6 @@
     <title>Shree Adhya Shakti Mataji</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Roboto|Lobster|Satisfy" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- <link rel="stylesheet" href="{{ asset('fonts/fontawesome.min.css') }}"> -->
@@ -23,6 +22,6 @@
     <!-- Bootstrap core JavaScript -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </body>
 </html>
